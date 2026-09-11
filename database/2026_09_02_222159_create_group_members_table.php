9437791d34f224face7e772babbf38a2c32c43d5
willyyanti248-svg <willyyanti248@gmail.com>
2026-09-11 09:10:16 +0700

    grup member

---
+++ database/2026_09_02_222159_create_group_members_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('group_members', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('users_id')
+                ->constrained('users')
+                ->cascadeOnDelete();
+
+            $table->foreignId('groups_id')
+                ->constrained('groups')
+                ->cascadeOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('group_members');
+    }
+};
